3770f9c06d70d26d63f0a37b33f17cde24979ed4
niltonfernandesj <niltonfernandesj@gmail.com>
2023-06-05 20:18:51 -0300

    Removing new method

--- src/person/Person.py
+++ src/person/Person.py
@@ -13,20 +13,20 @@ class Person:
 
         return self.weight / (self.height * self.height)
     
-    def getPersonBMIRange(self) -> str:
+    # def getPersonBMIRange(self) -> str:
 
-        person_BMI = self.calculateBMI()
+    #     person_BMI = self.calculateBMI()
 
-        if person_BMI < 18.5:
-            return "Baixo peso"
-        elif person_BMI >= 18.5 and person_BMI < 25:
-            return "Normal"
-        elif person_BMI >= 25 and person_BMI < 30:
-            return "Sobrepeso"
-        elif person_BMI >= 30 and person_BMI < 35:
-            return "Obesidade"
-        elif person_BMI >= 35 and  person_BMI < 40:
-            return "Obesidade grave"
-        else:
-            return "Obesidade muito grave"
+    #     if person_BMI < 18.5:
+    #         return "Baixo peso"
+    #     elif person_BMI >= 18.5 and person_BMI < 25:
+    #         return "Normal"
+    #     elif person_BMI >= 25 and person_BMI < 30:
+    #         return "Sobrepeso"
+    #     elif person_BMI >= 30 and person_BMI < 35:
+    #         return "Obesidade"
+    #     elif person_BMI >= 35 and  person_BMI < 40:
+    #         return "Obesidade grave"
+    #     else:
+    #         return "Obesidade muito grave"
 
